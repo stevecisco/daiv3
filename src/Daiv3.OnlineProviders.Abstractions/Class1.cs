a1670039f6e2f3b553bd04905c26dc5dec685384
steve.cisco <steve.cisco@centricconsulting.com>
2026-03-03 15:55:05 -0500

    Removed placeholder class

--- src/Daiv3.OnlineProviders.Abstractions/Class1.cs
+++
@@ -1,9 +0,0 @@
-namespace Daiv3.OnlineProviders.Abstractions;
-
-/// <summary>
-/// Placeholder class for Daiv3.OnlineProviders.Abstractions library.
-/// This library provides common provider interfaces.
-/// </summary>
-public class Class1
-{
-}
